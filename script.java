36b0915a488ddc859b352e75ee25fe46b8fafc09
jimmypuli <armg296@gmail.com>
2025-07-30 14:11:17 -0400

    setup

--- script.java
+++ script.java
@@ -4,7 +4,9 @@ public class script {
     public static void main(String[] args) {
         try(Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch(); 
-    
+            Page page = browser.newPage(); 
+            page.navigate("https://playwright.dev"); 
+            System.out.println(page.title());
         }
     }
 }
